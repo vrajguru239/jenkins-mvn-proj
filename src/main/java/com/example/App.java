cb190924758be56e05e2a0439ec3844a24e052b9
vrajguru239 <vaishnavirajguru123@gmail.com>
2025-03-04 14:02:36 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
+        System.out.println("Hello, jenkin!");
     }
 }
